3e63fbbe1440d9daa04e526b1fb4e914e279248a
Ozório <jleandro.dev@gmail.com>
2023-04-15 21:16:12 -0300

    Template html criado

--- jogoteca.py
+++ jogoteca.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        pass
 
 app = Flask(__name__)
 
 
 @app.route('/inicio')
 def ola():
-    return '<h1>Hello</h1>'
+    lista = ['Tetris', 'Skyrim', 'God of War']
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 app.run()
